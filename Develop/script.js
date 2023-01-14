6ae3f87e596fc6d3a0ac0584b1bc9288cd1e969d
Velcro <davesimpcontrol@gmail.com>
2023-01-14 16:22:19 -0700

    prompt and confirm interactions are now visible in the console log

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 const numbers = [... '1234567890'];
@@ -13,20 +14,12 @@ function passwordCriteria() {
   const passwordLength = prompt(
     "Choose password length between 8 and 128"
   );
-  console.log('${passwordLength} =');
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    const parameter = (
-      confirm("Press OK to add numbers. Press Cancel to continue without."),
-      confirm("Press OK to add lowercase. Press Cancel to continue without."),
-      confirm("Press OK to add uppercase. Press Cancel to continue without."),
-      confirm("Press OK to add special characters. Press Cancel to continue without.")
-    );
-  } else {
-    passwordCriteria()
-    return;
-  }
+  var number = confirm("Press OK to add numbers. Press Cancel to continue without.");
+  var lcAlphabet = confirm("Press OK to add lowercase. Press Cancel to continue without.");
+  var ucAlphabet = confirm("Press OK to add uppercase. Press Cancel to continue without.");
+  var sCharacters = confirm("Press OK to add special characters. Press Cancel to continue without.");
+  console.log(passwordLength, number, lcAlphabet, ucAlphabet, sCharacters);
 }
-
 
 
 // Write password to the #password input

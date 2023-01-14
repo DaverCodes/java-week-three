d8711a4d06611f1a9a1c882d5b26aca90df7c35c
Velcro <davesimpcontrol@gmail.com>
2023-01-14 12:10:45 -0700

    added const variables for all password character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const numbers = [... '1234567890'];
+const lcAlphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+const ucAlphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+const sCharacters = [..."~`!@#$%^&*()_-+={[}]|:;'<,>.?/"];
+console.log(sCharacters);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

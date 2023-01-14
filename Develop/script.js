82a125dc6c6125a568efd660f9baa37687b16214
Velcro <davesimpcontrol@gmail.com>
2023-01-14 15:19:03 -0700

    added all prompts, added click to begin prompts

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,30 @@ var generateBtn = document.querySelector("#generate");
 const numbers = [... '1234567890'];
 const lcAlphabet = [...'abcdefghijklmnopqrstuvwxyz'];
 const ucAlphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-const sCharacters = [..."~`!@#$%^&*()_-+={[}]|:;'<,>.?/"];
+// make sure '"' works
+const sCharacters = [...'"', " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
 console.log(sCharacters);
+
+
+function passwordCriteria() {
+  const passwordLength = prompt(
+    "Choose password length between 8 and 128"
+  );
+  console.log('${passwordLength} =');
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    const parameter = (
+      confirm("Press OK to add numbers. Press Cancel to continue without."),
+      confirm("Press OK to add lowercase. Press Cancel to continue without."),
+      confirm("Press OK to add uppercase. Press Cancel to continue without."),
+      confirm("Press OK to add special characters. Press Cancel to continue without.")
+    );
+  } else {
+    passwordCriteria()
+    return;
+  }
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -17,4 +39,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+generateBtn.addEventListener("click", passwordCriteria);
+
 generateBtn.addEventListener("click", writePassword);

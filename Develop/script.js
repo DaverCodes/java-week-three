9f4fd0c72dde8c09c5752c79bc6deb8a8ab5fdee
Velcro <davesimpcontrol@gmail.com>
2023-01-18 15:38:49 -0700

    cleaned up code and nearly done

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,15 @@
 // Assignment Code
 
 var generateBtn = document.querySelector("#generate");
+var combinedTrue = '';
+var password = '';
 
-// const numbers = [... '1234567890'];
-// const lcAlphabet = [...'abcdefghijklmnopqrstuvwxyz'];
-// const ucAlphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-// // make sure '"' works the special key \ is not visible in the string
-// const sCharacters = [...'"', " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
-let combinedTrue = '';
-
-
-
-
-function passwordCriteria() {
-  let passwordLength = prompt(
+function generatePassword() {
+  var passwordLength = prompt(
     "Choose password length between 8 and 128"
   );
+  
+  
   if (passwordLength < 8) {
     alert('this password is too short')
     alert('choose a number greater than 8')
@@ -27,50 +21,42 @@ function passwordCriteria() {
     alert("let's try this again")
     return
   }
-
-
-  const numbers = confirm("Press OK to add numbers. Press Cancel to continue without.");
-  const lcAlphabet = confirm("Press OK to add lowercase. Press Cancel to continue without.");
-  const ucAlphabet = confirm("Press OK to add uppercase. Press Cancel to continue without.");
-  const sCharacters = confirm("Press OK to add special characters. Press Cancel to continue without.");
+  //parseinit takes the argument introduced in the prompt and changes the boolean into an integer thats specifically used to define SCALE
+  passwordLength = parseInt(passwordLength)
+  
+  var numbers = confirm("Press OK to add numbers. Press Cancel to continue without.");
+  var lcAlphabet = confirm("Press OK to add lowercase. Press Cancel to continue without.");
+  var ucAlphabet = confirm("Press OK to add uppercase. Press Cancel to continue without.");
+  var sCharacters = confirm("Press OK to add special characters. Press Cancel to continue without.");
   console.log(passwordLength, numbers, lcAlphabet, ucAlphabet, sCharacters);
+  
+  
+  if (!numbers && !lcAlphabet && !ucAlphabet && !sCharacters) {
+    alert('you gotta choose something')
+    alert('choose at least one prompt')
+    alert('please')
+  }  
   
   if (numbers) combinedTrue += [... '1234567890'];
   if (lcAlphabet) combinedTrue += [...'abcdefghijklmnopqrstuvwxyz'];
   if (ucAlphabet) combinedTrue += [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
   if (sCharacters) combinedTrue += [...'"', " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"]
   console.log(combinedTrue);
-  
-  if (!numbers && !lcAlphabet && !ucAlphabet && !sCharacters) {
-    alert('you gotta choose something')
-    alert('choose at least one prompt')
-    alert('please')
+  const ultimateTrue = [combinedTrue]
+
+  const shuffledCombo = ultimateTrue.sort((a,b) => 0.5 - Math.random())
+  console.log(shuffledCombo);
+
+  for (let i = 0; i < passwordLength; i++) {
+    var passwordShuffle = Math.floor(Math.random() * combinedTrue.length);
+    password = combinedTrue[passwordShuffle];
   }
+
+return password
+
+  
 }
-// this is a really fun bit of code
-  // const passwordLength = prompt(
-  //   "Choose password length between 8 and 128"
-  // );
-  //if number = true then combinedTrue = number.concat
 
-
-  // function (checkRequirments) {
-  //   if (number) { 
-
-    // var hasNumber = /\d/;
-      
-  //   } else {
-      
-  //   }
-    
-  // }
-
-// const combined = functoion
-// if const=true add const to combined
-// pword = pwordNum + pwordAlphaUp + pwordAlphaLow + pwordChar
-
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -80,18 +66,6 @@ function writePassword() {
 }
 
 
-// function random_item(items)
-// {
-  
-// return items[Math.floor(Math.random()*items.length)];
-     
-// }
-
-// console.log(random_item(numbers));
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", passwordCriteria);
+generateBtn.addEventListener("click", generatePassword);
 
 generateBtn.addEventListener("click", writePassword);
